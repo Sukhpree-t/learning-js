3cf42791b7c5fc860d146876933131608802471a
Sukhpree-t <111574371+Sukhpree-t@users.noreply.github.com>
2025-04-24 12:15:20 +0000

    var done

--- basics/01_var.js
+++ basics/01_var.js
@@ -7,3 +7,4 @@ console.table([acccountID,accountEmail,accountPassword])
 // Prefer note use var
 // because of issue in block scope and functional scope
 
+alert(3+3) we dont use because this is nodejs not browser 
