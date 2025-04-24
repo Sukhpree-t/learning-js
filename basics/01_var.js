df0ce94673634c13717a5fcfe6f6c23d7254eead
Sukhpree-t <111574371+Sukhpree-t@users.noreply.github.com>
2025-04-24 11:39:29 +0000

    container config done

---
+++ basics/01_var.js
@@ -0,0 +1,8 @@
+const acccountID = 11223344;
+let accountEmail = "ssing@mgail.com";
+var accountPassword = "11223344";
+
+console.table([acccountID,accountEmail,accountPassword])
+
+// Prefer note use var
+// because of issue in block scope and functional scope
